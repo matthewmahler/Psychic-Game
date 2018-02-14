8800cd00bc1a8f195cbcd0b65e7f80b6cdbaced7
matthewmahler <matthewjamesmahler@gmail.com>
2018-02-14 10:49:18 -0500

    little changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,14 @@
   var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
   var wins = 0;
   var losses = 0;
   var lives = 10;
-
 
   document.onkeyup = function (event) {
 
     var userGuess = event.key;
 
-
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
 
     if (userGuess === computerGuess) {
       wins++;
@@ -20,12 +16,6 @@
       losses++;
       lives--;
     }
-      
-
-    
-
-
-
 
     var html =
       "<p>You chose: " + userGuess + "</p>" +
@@ -33,7 +23,6 @@
       "<p>wins: " + wins + "</p>" +
       "<p>losses: " + losses + "</p>" +
       "<p>lives: " + lives + "</p>";
-
 
     document.querySelector("#game").innerHTML = html;
   }
